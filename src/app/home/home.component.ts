b675462d35fb05f957328bf2002c7d655e1d4fcf
ivsonsantos <ivson.java@gmail.com>
2023-11-06 19:11:51 +0100

    Criacao dos htmls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,17 +10,7 @@ import { Housinglocation } from '../housinglocation';
     CommonModule,
     HousingLocationComponent
   ],
-  template: `
-   <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
-  </section>
-  `,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
